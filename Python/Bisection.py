506ec5e041e5065909b1dd697cbf4f75ba35b431
Nishan Rowjee <>
2022-04-30 14:49:43 +0200

    Added Python Folder

---
+++ Python/Bisection.py
@@ -0,0 +1,9 @@
+#This project attempts to find the roots of a given function using the bisection theorem to within a specified accuracy
+
+print("Hello World")
+print("Please enter endpoints of interval")
+x1 = input("Left Endpoint: ")
+x2 = input("Right Endpoint: ")
+
+def Bisection():
+    
